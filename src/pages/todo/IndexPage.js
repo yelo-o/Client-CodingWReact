9d903624c507281c23aed2006fcb9b4353aeaa31
yelo-o <flyordig@gmail.com>
2024-02-10 18:05:40 +0900

    경로 처리를 위한 useParams()

--- src/pages/todo/IndexPage.js
+++ src/pages/todo/IndexPage.js
@@ -1,12 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import BasicLayout from "../../layouts/BasicLayout";
+import { useCallback } from "react";
 
 const IndexPage = () => {
+    const Navigate = useNavigate()
+
+    const handleClickList = useCallback(() => {
+        Navigate({ pathname:'list'});
+    })
+    
+    const handleClickAdd = useCallback(() => {
+        Navigate({ pathname:'add'});
+    })
     return (
         <BasicLayout>
             <div className="w-full flex m-2 p-2 ">
-                <div className="text-xl m-1 p-2 w-20 front-extrabold text-center underline">LIST</div>
-                <div className="text-xl m-1 p-2 w-20 front-extrabold text-center underline">ADD</div>
+                <div className="text-xl m-1 p-2 w-20 front-extrabold text-center underline"
+                onClick={handleClickList}>LIST</div>
+                <div className="text-xl m-1 p-2 w-20 front-extrabold text-center underline"
+                onClick={handleClickAdd}>ADD</div>
             </div>
             <div className="flex flex-wrap w-full">
                 <Outlet/>
