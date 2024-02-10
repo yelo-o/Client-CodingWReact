9f81eb8ebf045b3043f09399373e1b4b2dd97bdd
yelo-o <flyordig@gmail.com>
2024-02-10 18:36:29 +0900

    수정/삭제 처리 및 페이지 이동

--- src/router/todoRouter.js
+++ src/router/todoRouter.js
@@ -5,6 +5,7 @@ const Loading = <div>Loading...</div>
 const TodoList = lazy(() => import("../pages/todo/ListPage"))
 const TodoRead = lazy(() => import("../pages/todo/ReadPage"))
 const TodoAdd = lazy(() => import("../pages/todo/AddPage"))
+const TodoModify = lazy(() => import("../pages/todo/ModifyPage"))
 
 const todoRouter = () => {
 
@@ -24,7 +25,11 @@ const todoRouter = () => {
         {
             path: "add",
             element: <Suspense fallback={Loading}><TodoAdd/></Suspense>
-        }
+        },
+        {
+            path: "modify/:tno",
+            element: <Suspense fallback={Loading}><TodoModify/></Suspense>
+        },
     ]
 }
 
