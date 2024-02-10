13bbbd9121628918fa263376cb2068bf9cc0f9ff
yelo-o <flyordig@gmail.com>
2024-02-10 16:27:38 +0900

    하위 경로의 설정과 <Outlet>

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -3,15 +3,18 @@ import BasicMenu from "../components/menus/BasicMenu";
 const BasicLayout = ({children}) => {
     return (
         <>
-        <BasicMenu></BasicMenu>
-        <div className="bg-white my-5 w-full flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+        {/* 기존 헤더 대신 BasicMenu */}
+        <BasicMenu/>
+
+        {/* 상단 여백 my-5 제거 */}
+        <div className="bg-white w-full flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
             <main className="bg-sky-300 md:w-2/3 lg:w-3/4 px-5 py-40">
+            {/* 상단 여백 py-40 변경 flex 제거 */}
                 {children}
             </main>
-            <aside className="bg-green-300 md:w-1/3 lg:w-1/4 px-5 py-40">
-                <h1 className="text-2xl md:text-4xl">
-                    Sidebar
-                </h1>
+            <aside className="bg-green-300 md:w-1/5 lg:w-1/4 px-5 flex py-5">
+            {/* py-40 -> py-5 변경 flex 추가 */}
+                <h1 className="text-2xl md:text-4xl">Sidebar</h1>
             </aside>
         </div>
         </>
--- src/router/root.js
+++ src/router/root.js
@@ -7,6 +7,8 @@ const Main = lazy(() => import("../pages/MainPage"))
 
 const About = lazy(() => import("../pages/AboutPage"))
 
+const TodoIndex = lazy(() => import("../pages/todo/IndexPage"))
+
 const root = createBrowserRouter([
     {
         path: "",
@@ -15,7 +17,11 @@ const root = createBrowserRouter([
     {
         path: "about",
         element: <Suspense fallback={Loading}><About/></Suspense>
-    }
+    },
+    {
+        path: "todo",
+        element: <Suspense fallback={Loading}><TodoIndex/></Suspense>
+    },
 ])
 
 export default root;
