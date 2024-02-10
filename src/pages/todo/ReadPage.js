3befe3ccb92a8097ffb374f9f073d5fa7180a662
yelo-o <flyordig@gmail.com>
2024-02-10 18:24:58 +0900

    동적 페이지 이동 - querystring 유지

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,11 +1,31 @@
-import { useParams } from "react-router-dom";
+import { useCallback } from "react";
+import { createSearchParams, useNavigate, useParams, useSearchParams } from "react-router-dom";
 
 const ReadPage = () => {
-    const {tno} = useParams();
+    const {tno} = useParams()
+
+    const navigate = useNavigate()
+
+    const [queryParams] = useSearchParams()
+
+    const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1
+    const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10
+
+    const queryStr = createSearchParams({page, size}).toString()
+
+    const moveToModify = useCallback((tno) => {
+        navigate({
+            pathname:`/todo/modify/${tno}`,
+            search: queryStr
+        })
+    }, [tno ,page, size])
 
     return (
         <div className="text-3xl font-extrabold">
             Todo Read Page Component {tno}
+            <div>
+                <button onClick={() => moveToModify(tno)}>Test Modify</button>
+            </div>
         </div>
     );
 }
